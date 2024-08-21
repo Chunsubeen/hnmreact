6bc35089f2dc31fc7cba0dac21a5d25e22ebac11
Subeen Chun <petitloutre@naver.com>
2024-08-22 02:22:44 +0900

    first commit

---
+++ src/component/ProductCard.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const ProductCard = () => {
+    return (
+        <div>
+            <img src="https://cdn.gjdream.com/news/photo/202208/617735_218065_3934.jpg"/>
+            <div>Conscious choice</div>
+            <div>벨티드 트월 코트</div>
+            <div>1원</div>
+            <div>신제품</div>
+        </div>
+    )
+}
+
+export default ProductCard
